76629fd0499b2bceb0185b97b88605867fe71c18
RODRB153 <bianca.rodrigues@disney.com>
2023-09-28 09:52:53 -0300

    parei em PostgreSQL com Docker

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from 'fastify';
 
+import { PrismaClient } from '@prisma/client'
+
 export const app = fastify();
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        //id: 1 - posso passar o ID se eu desejar.
+        name: 'Bianca Rodrigues',
+        email: 'biancar1987@gmail.com'
+    }
+})
